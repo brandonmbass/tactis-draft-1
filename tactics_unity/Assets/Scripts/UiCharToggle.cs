e179257fc4e8c22cc80d34edbcc52fd0156f8949
rollie42 <git@rolnicki.net>
2016-05-03 13:22:54 +0900

    UI

---
+++ tactics_unity/Assets/Scripts/UiCharToggle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiCharToggle : Toggle
+{
+    public Image BackgroundImage;
+
+    protected override void Start()
+    {
+        base.Start();
+        BackgroundImage = GetComponent<Image>();
+    }
+
+    public void Toggle(bool isToggled) {
+        if (isToggled)
+        {
+            BackgroundImage.color = new Color32(18, 184, 39, 92);
+        }
+        else
+        {
+            BackgroundImage.color = new Color32(129, 129, 129, 92);
+        }        
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        
+    }
+}
