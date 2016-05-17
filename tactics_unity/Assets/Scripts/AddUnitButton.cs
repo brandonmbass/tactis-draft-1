b741c1dc4b5fb7975f1110ef58ea9f6c48582eb5
rollie42 <git@rolnicki.net>
2016-05-17 21:39:12 +0900

    Added buttons

---
+++ tactics_unity/Assets/Scripts/AddUnitButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddUnitButton : MonoBehaviour {
+
+    public Button Button;
+    public BattlefieldController BattlefieldController;
+
+	// Use this for initialization
+	void Start () {
+        Button.onClick.AddListener(() => { BattlefieldController.PushState<UnitAdderState>(); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
